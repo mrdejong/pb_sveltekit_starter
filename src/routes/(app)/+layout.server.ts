71fc1f1ae8797ad00f13272f6698460b0114e1c1
Alexander de Jong <mrdejong89@gmail.com>
2025-06-03 22:12:28 +0200

    We shouldn't leave unused code around

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (async ({ locals, url }) => {
+export const load = (async ({ locals }) => {
     if (locals.user === null && !locals.pb.authStore.isValid) {
         return redirect(303, '/login');
     }
