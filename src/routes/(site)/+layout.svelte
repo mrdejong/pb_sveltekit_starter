0e625f982b158c1f1c4dbc14658a2fe56bfb0927
Alexander de Jong <alexander@dutchcaffeine.nl>
2025-09-25 20:47:43 +0200

    Make a cool welcome page

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -22,56 +22,29 @@
 	];
 </script>
 
-<div class="bg-base-100 shadow-sm">
-	<div class="container mx-auto">
-		<div class="navbar">
-			<div class="navbar-start">
-				<div class="dropdown">
-					<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4 6h16M4 12h8m-8 6h16"
-							/>
-						</svg>
-					</div>
-					<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-					<ul
-						tabindex="0"
-						class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-					>
-						<NavItems items={navItems} />
-					</ul>
-				</div>
-
-				<a href="/" class="btn btn-ghost text-xl">Habitor</a>
-			</div>
-			<div class="navbar-center hidden lg:flex">
-				<ul class="menu menu-horizontal px-1">
-					<NavItems items={navItems} />
-				</ul>
-			</div>
-			<div class="navbar-end gap-4">
-				{#if data.user !== null}
+<div class="hero bg-base-200 min-h-screen">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+		<img src="/img/bird.jpg" alt="Bird" class="max-w-sm rounded-lg shadow-2xl" />
+		<div>
+			<h1 class="text-5xl font-bold">Box Office News!</h1>
+			<p class="py-6">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<p class="pb-6">Please edit me in src/routes/(site)/+layout.svelte</p>
+			{#if data.user !== null}
+				<div class="flex gap-3 items-center">
+					<a class="link" href="/db">Dashboard</a>
 					<form method="POST" action="/logout">
 						<button type="submit" class="btn btn-sm btn-error">Logout</button>
 					</form>
-				{:else}
-					<div class="join">
-						<a class="join-item btn btn-sm" href="/login">Login</a>
-						<a class="join-item btn btn-primary btn-sm" href="/register">Register</a>
-					</div>
-				{/if}
-				<ThemeSwitcher />
-			</div>
+				</div>
+			{:else}
+				<div class="join">
+					<a class="join-item btn btn-sm" href="/login">Login</a>
+					<a class="join-item btn btn-primary btn-sm" href="/register">Register</a>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
